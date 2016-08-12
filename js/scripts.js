7f865b0060b28b110551413574e744ff4ad900ad
Angie Smith <avksmit2@gmail.com>
2016-08-12 16:21:16 -0700

    receive date from user of class start and calculate the suggested track depending on the start date

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,71 @@
 $(document).ready(function() {
   var drupal = parseInt(0)
   var android = parseInt(0)
+  var net = parseInt(0)
+  var rails = parseInt(0)
   $("#answers").submit(function() {
-    if ($("#design option:selected").text() === "yes") {
+    var startDate = new Date($("#classDate").val());
+    var startDate = parseInt(startDate.getMonth())+2;
+    var design = $("#design option:selected").text();
+    if (startDate === 8 && design === "Yes") {
+        drupal += 1;
+    } else if (startDate === 8 && design === "No") {
+      android +=1;
+    } else if (startDate === 11 && design === "Yes") {
+      rails += 1;
+    } else net += 1;
+    var data = $("#data option:selected").text();
+    if (startDate === 8 && data === "Yes") {
       drupal += 1;
-    } else android += 1;
-    if ($("#data option:selected").text() === "yes") {
+    } else if (startDate === 8 && data === "No") {
+      android += 1;
+    } else if (startDate === 11 && data === "Yes") {
+      net += 1;
+    } else rails += 1;
+    var company = $("input[name=companyType]:checked").val();
+    if (startDate === 8 && company === "large" || company === "start-up") {
+      android += 1;
+    } else if (startDate === 8 && company === "small") {
       drupal += 1;
-    } else android += 1;
-
-    if ($("input[name=companyType]:checked").val() === "large" || $("input[name=companyType]:checked").val() === "start-up") {
+    } else if (startDate === 11 && company === "small" && company === "start-up"){
+      rails += 1;
+    } else net += 1;
+    var work = $("input[name=workType]:checked").val()
+    if (startDate === 8 && work === "phone" || work === "business") {
       android += 1;
-    } else drupal += 1;
-    if ($("input[name=workType]:checked").val() === "phone" || $("input[name=workType]:checked").val() === "business") {
+    } else if (startDate === 8 && work === "websites") {
+      drupal += 1;
+    } else if (startDate === 11 && work === "phone" || work === "business") {
+      net += 1;
+    } else rails += 1;
+    var background = $("input[name=backgroundLanguages]:checked").val();
+    if (startDate === 8 && background === "C#" || background === "VB") {
       android += 1;
-    } else drupal += 1;
-    if ($("input[name=backgroundLanguages]:checked").val() === "C#" || $("input[name=backgroundLanguages]:checked").val() === "VB") {
-      android += 1;
-    } else drupal += 1;
+    } else if (startDate === 8 && background === "html") {
+      drupal +=1;
+    } else if (startDate === 11 && background === "C#" || background === "VB") {
+      net += 1;
+    } else rails += 1;
     var fName = $("#fname").val();
     var lName = $("#lname").val();
     $(".firstName").text(fName);
     $(".lastName").text(lName);
-    if (drupal > android) {
+    if (startDate === 8 && drupal > android) {
       var result = "PHP / Drupal"
-    } else {
+    } else if (startDate === 8 && drupal < android) {
       var result = "Java / Android"
-    }
-    $(".suggestion").text(result)
-  event.preventDefault();
+    } else if (startDate === 11 && net > rails) {
+      var result = "C# / .Net"
+    } else var result = "Ruby / Rails";
+    $(".suggestion").text(result);
+    $("#result").show();
+    event.preventDefault();
+  });
+  $('form').on('reset', function(event) {
+    $("#result").hide();
+    var drupal = parseInt(0)
+    var android = parseInt(0)
+    var net = parseInt(0)
+    var rails = parseInt(0)
   });
 });
